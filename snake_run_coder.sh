163d6f692952030e9031b630f14366a2552b700a
buyn <buyn.tex@gmail.com>
2026-03-21 08:04:57 +0200

    feat: use nemotron-3-super-120b-a12b and add weak model nemotron-nano-9b-v2

--- snake_run_coder.sh
+++ snake_run_coder.sh
@@ -1,14 +1,14 @@
 python -m unittest discover -s snake/tests 2>&1 | tee snake/last_unittest_run.log
-# working
-# aider --model openrouter/stepfun/step-3.5-flash:free \
-# aider --model openrouter/nvidia/nemotron-3-super-120b-a12b:free \
 # crash
 # aider --model openrouter/arcee-ai/trinity-large-preview:free \
 #limit
 # aider --model openrouter/meta-llama/llama-3.3-70b-instruct:free \
 # aider --model openrouter/z-ai/glm-4.5-air:free \
 # aider --model openrouter/qwen/qwen3-coder:free \
-aider --model openrouter/stepfun/step-3.5-flash:free \
+# working
+# aider --model openrouter/stepfun/step-3.5-flash:free \
+# aider --model openrouter/nvidia/nemotron-3-super-120b-a12b:free \
+aider --model openrouter/nvidia/nemotron-3-super-120b-a12b:free \
     --weak-model openrouter/nvidia/nemotron-nano-9b-v2:free \
     --read snake/technical_specification.md \
     --read snake/run.md \
