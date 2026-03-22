b7bc3fe5216803758512c34a919cbdfe52a66272
buyn <buyn.tex@gmail.com>
2026-03-22 14:26:07 +0200

    runnig app

--- snake_run_arhitect.sh
+++ snake_run_arhitect.sh
@@ -20,7 +20,7 @@ echo "" >> snake/run.md
 
 # aider --model openrouter/nvidia/nemotron-3-super-120b-a12b:free \
 aider --model openrouter/stepfun/step-3.5-flash:free \
-    --weak-model openrouter/nvidia/nemotron-nano-9b-v2:free \
+    --weak-model nvidia_nim/meta/llama3-8b-instruct \
     --read snake/last_unittest_run.log \
     --read snake/technical_specification.md \
     --read snake/python_code_style.md \
@@ -36,6 +36,8 @@ aider --model openrouter/stepfun/step-3.5-flash:free \
     --exit 
 
     # --weak-model groq/llama-3.1-8b-instant \
+    # --weak-model nvidia_nim/meta/llama3-8b-instruct \
+    # --weak-model openrouter/nvidia/nemotron-nano-9b-v2:free \
     # --weak-model openrouter/mistralai/mistral-small-3.1-24b-instruct:free \
     # --notifications-command "" \
 python -m unittest discover -s snake/tests 2>&1 | tee snake/last_unittest_run.log
--- snake_run_coder.sh
+++ snake_run_coder.sh
@@ -2,8 +2,10 @@
 notify-send "aider Coder session Started" "Coder session is Started"
 echo "Coder session is Started"
 python -m unittest discover -s snake/tests 2>&1 | tee snake/last_unittest_run.log
+echo "Coder session is Starting" >> snake/last_unittest_run.log
 # crash
 # aider --model openrouter/arcee-ai/trinity-large-preview:free \
+# aider --model openrouter/openai/gpt-oss-120b:free \
 #limit
 # aider --model openrouter/meta-llama/llama-3.3-70b-instruct:free \
 # aider --model openrouter/z-ai/glm-4.5-air:free \
@@ -14,8 +16,8 @@ python -m unittest discover -s snake/tests 2>&1 | tee snake/last_unittest_run.lo
 
 # aider --model gemini/gemini-flash-lite-latest \
 
-aider --model openrouter/meta-llama/llama-3.3-70b-instruct:free \
-    --weak-model openrouter/nvidia/nemotron-nano-9b-v2:free \
+aider --model openrouter/stepfun/step-3.5-flash:free \
+    --weak-model nvidia_nim/meta/llama3-8b-instruct \
     --read snake/technical_specification.md \
     --read snake/run.md \
     --read snake/python_code_style.md \
@@ -28,6 +30,7 @@ aider --model openrouter/meta-llama/llama-3.3-70b-instruct:free \
     --auto-test \
     --exit 
 
+    # --weak-model openrouter/nvidia/nemotron-nano-9b-v2:free \
     # --weak-model openrouter/mistralai/mistral-small-3.1-24b-instruct:free \
     # --weak-model groq/llama-3.1-8b-instant \
     # --yes-always \
